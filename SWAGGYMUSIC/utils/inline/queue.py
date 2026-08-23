b696fa5b69114d354e43b5a28243d2103017e308
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-08-23 16:47:25 +0530

    Add files via upload

--- SWAGGYMUSIC/utils/inline/queue.py
+++ SWAGGYMUSIC/utils/inline/queue.py
@@ -1,5 +1,4 @@
 from typing import Union
-
 from SWAGGYMUSIC import app
 from SWAGGYMUSIC.utils.formatters import time_to_seconds
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
@@ -25,8 +24,7 @@ def queue_markup(
             ),
         ]
     ]
-
-    duration_buttons = [
+    dur = [
         [
             InlineKeyboardButton(
                 text=_["QU_B_2"].format(played, dur),
@@ -44,47 +42,37 @@ def queue_markup(
             ),
         ],
     ]
-
-    return InlineKeyboardMarkup(
-        not_dur if DURATION == "Unknown" else duration_buttons
-    )
+    upl = InlineKeyboardMarkup(not_dur if DURATION == "Unknown" else dur)
+    return upl
 
 
 def queue_back_markup(_, CPLAY):
-    buttons = [
+    upl = InlineKeyboardMarkup(
         [
-            InlineKeyboardButton(
-                text=_["BACK_BUTTON"],
-                callback_data=f"queue_back_timer {CPLAY}",
-            ),
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data="close",
-            ),
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"queue_back_timer {CPLAY}",
+                ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close",
+                ),
+            ]
         ]
-    ]
-
-    return InlineKeyboardMarkup(buttons)
+    )
+    return upl
 
 
 def aq_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="• ᴊσɪη ησω •",
-                url="https://t.me/messo_network",
+                text="• ᴊσɪη ησω •", url=f"https://t.me/messo_network"
             ),
             InlineKeyboardButton(
-                text="• ɢʀᴏᴜᴘ ᴄʜᴀᴛ •",
-                url="https://t.me/+Gi7KEHVRUNlhZjg1",
+                text="• ɢʀᴏᴜᴘ ᴄʜᴀᴛ •", url=f"https://t.me/+Gi7KEHVRUNlhZjg1"
             ),
         ],
-        [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data="close",
-            )
-        ],
-    ]
-
-    return InlineKeyboardMarkup(buttons)
+      ]
+    return buttons
